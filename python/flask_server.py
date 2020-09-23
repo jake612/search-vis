14cd0270c2820f34e8080db829db6add36dfbd10
jake612 <jake612@live.unc.edu>
2020-09-23 10:23:44 -0400

    Added ability to query db

---
+++ python/flask_server.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, jsonify
+import re
+import requests
+app = Flask(__name__)
+
+@app.route('/')
+def homePage():
+    return render_template('index.html')
+
+@app.route('/<query>')
+def queryServer(query):
+    print(query)
+    #Stry:
+    if re.match("^[A-Za-z\s]+$", query):
+        queryTerms = query.split(' ')
+        requestUrl = 'http://10.4.80.108:8984/solr/MEDLINEv6/select'
+        params = {
+            "q": "{titleQueries} OR {abstractQueries}".format(titleQueries= " OR ".join(['title:' + i for i in queryTerms]), abstractQueries=" OR ".join(['abstract:' + i for i in queryTerms])).encode('utf-8'),
+            "rows": "50"
+        }
+        r = requests.get(requestUrl, params=params)
+        return jsonify(r.json())
+    return 'Error: Query can only contain letters and spaces'
+
+    #except Exception as e:
+    #    return str(e)
+
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- python/loadJSON.py
+++ python/loadJSON.py
@@ -1,5 +1,4 @@
 # Python file for loading a relationship graph from JSON
-import sys
 import json
 
 def stringToArr(string):
@@ -12,20 +11,24 @@ def solrResponseJSON(response):
     return json.load(response)['response']['docs']
 
 def loadJSONArray(jsonArr):
-    timesSeen = dict()
+    nodes = dict()
     links = dict()
     for obj in jsonArr:
         codes = stringToArr(obj['abstract_snomed_ents'])
-        print(obj)
-        print(codes)
+        for code in codes:
+            if len(code) < 4:
+                break
+            nodes[code[2]] = {
+                name: code[3],
+                timesSeen: 1,
+            } if node[2] not in nodes else nodes[code[2]].timesSeen += 1
         for idx, code in enumerate(codes):
             if len(code) < 4:
                 break
-            timesSeen[code[2]] = 1 if code[2] not in timesSeen else timesSeen[code[2]] + 1
+            node[code[2]] = {} if node[2] not in timesSeen else timesSeen[code[2]] + 1
             offset = idx + 1
             while offset < len(codes):
-                print(codes[offset])
-                if int(codes[offset][0]) - int(code[1]) > 10:
+                if int(codes[offset][0]) - int(code[1]) <= 10:
                     break
                 else:
                     link = (code[2], codes[offset][2])
@@ -39,8 +42,3 @@ def loadJSONArray(jsonArr):
                     offset += 1
 
     return timesSeen, links
-
-
-if __name__ == '__main__':
-    with open('headache.txt', 'r') as file:
-        print(loadJSONArray(solrResponseJSON(file)))
