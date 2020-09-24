b5ba55e41cd37a9fbc504baed94f9e4bc9d1b559
jake612 <jake612@live.unc.edu>
2020-09-23 23:37:03 -0400

    Added functioning endpoint that queries server then returns json with nodes and links

--- python/loadJSON.py
+++ python/loadJSON.py
@@ -7,25 +7,33 @@ def stringToArr(string):
         return_arr.append(ent.split(', '))
     return return_arr
 
-def solrResponseJSON(response):
-    return json.load(response)['response']['docs']
 
-def loadJSONArray(jsonArr):
+def dictsToJSONResp(nodes, links):
+    return {
+        "nodes": [{"code": k, "timesSeen": v["timesSeen"], "name": v["name"]} for k, v in nodes.items()],
+        "links": [{"source": k[0], "target": k[1], "timesSeen": v } for k, v in links.items()]
+    }
+
+def loadJSONArray(jsonResp):
     nodes = dict()
     links = dict()
-    for obj in jsonArr:
+    for obj in jsonResp['response']['docs']:
         codes = stringToArr(obj['abstract_snomed_ents'])
         for code in codes:
             if len(code) < 4:
                 break
-            nodes[code[2]] = {
-                name: code[3],
-                timesSeen: 1,
-            } if node[2] not in nodes else nodes[code[2]].timesSeen += 1
+            if code[2] in nodes:
+                nodes[code[2]]["timesSeen"] += 1
+            else:
+                nodes[code[2]] = {
+                    "name": code[3],
+                    "timesSeen": 1,
+                 }
+
+
         for idx, code in enumerate(codes):
             if len(code) < 4:
                 break
-            node[code[2]] = {} if node[2] not in timesSeen else timesSeen[code[2]] + 1
             offset = idx + 1
             while offset < len(codes):
                 if int(codes[offset][0]) - int(code[1]) <= 10:
@@ -41,4 +49,4 @@ def loadJSONArray(jsonArr):
                         links[link] = 1
                     offset += 1
 
-    return timesSeen, links
+    return dictsToJSONResp(nodes, links)
