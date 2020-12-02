5eace15179aa01b48f1304f6b6e425da9d890499
jake612 <jake612@live.unc.edu>
2020-12-02 15:34:02 -0500

    Added colors to the nodes and ability to flick back and forth between categories

--- python/flask_server.py
+++ python/flask_server.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, jsonify
-import re
-import requests
-import pickle
+import pickle, json, requests, re
 from loadJSON import *
 app = Flask(__name__)
 
@@ -14,23 +12,37 @@ def queryServer(query):
     #Stry:
     if re.match("^[A-Za-z\s]+$", query):
         queryTerms = query.split(' ')
-        requestUrl = 'http://10.4.80.108:8984/solr/MEDLINEv6/select'
+        requestUrl = 'http://10.4.80.108:8983/solr/MEDLINEv6/query' # Hardcoded server address
+
+        # Query is built by taking each individual word in query and searching for it in the title or abstract field
         params = {
             "q": "{titleQueries} OR {abstractQueries}".format(titleQueries= " OR ".join(['title:' + i for i in queryTerms]), abstractQueries=" OR ".join(['abstract:' + i for i in queryTerms])).encode('utf-8'),
             "rows": "50"
         }
-        print(params)
         r = requests.get(requestUrl, params=params)
-        ret = loadJSONArray(r.json(), SNOMEDLinks)
+        ret = loadJSONArray(r.json(), SNOMEDLinks, semanticTypes)
         return jsonify(ret)
     else:
         return 'Error: Query can only contain letters and spaces'
 
-    #except Exception as e:
-    #    return str(e)
 
+# We are using the semantic categories for coloring the nodes displayed by the graph
+# This function takes the JSON and converts it to a lookup dictionary for the loadJSONArray function to use
+# Each key is a type code and each entry is the category it belongs to
+# Argument is the path/name of the file to load
+def semanticTypesLoader(path):
+    returnDict = dict()
+    with open(path, 'r') as jsonFile:
+        data = json.load(jsonFile)
+        for categoryList in data.values():
+            for entry in categoryList:
+                vals = entry.split('|')
+                returnDict[vals[2]] = vals[0]
     
+    return returnDict
+ 
 
 if __name__ == "__main__":
-    SNOMEDLinks = pickle.load(open('../data/edges.p', 'rb'))
+    SNOMEDLinks = pickle.load(open('../data/edges.p', 'rb')) # SNOMED Links is a pre-processed set of SNOMED codes for the purpose of checking if they are in the ontology
+    semanticTypes = semanticTypesLoader('../data/ctakes_semantic_types.json')
     app.run(debug=True)
--- python/loadJSON.py
+++ python/loadJSON.py
@@ -1,52 +1,66 @@
 # Python file for loading a relationship graph from JSON
 import json
 import re
+import math
 
+# Helper for properly splitting the abstract_snomed_ents field
 def stringToArr(string):
     return_arr = []
     for ent in string[1:-1].split('], ['):
         return_arr.append(ent.split(', '))
     return return_arr
 
+# Magic number 50: only returning the first 50 results for now
+def calcPMI(node1, node2, timesSeen):
+    return math.log2(50*timesSeen/(node1["timesSeen"] * node2["timesSeen"]))
 
+
+# Format info for response
 def dictsToJSONResp(nodes, links, overlap):
     return {
-        "nodes": [{"code": k, "timesSeen": v["timesSeen"], "name": v["name"]} for k, v in nodes.items()],
-        "links": [{"source": k[0], "target": k[1], "timesSeen": v } for k, v in links.items()],
+        "nodes": [{"code": k, "timesSeen": v["timesSeen"], "name": v["name"], "type": v["type"]} for k, v in nodes.items()],
+        "links": [{"source": k[0], "target": k[1], "timesSeen": v , "pmi": calcPMI(nodes[k[0]], nodes[k[1]], v)} for k, v in links.items()],
         "overlap": [{"source": k[0], "target": k[1], "timesSeen": 1} for k in overlap]
     }
 
-def loadJSONArray(jsonResp, SNOMEDLinks):
+# Main function called from flask_server to calculate nodal information
+def loadJSONArray(jsonResp, SNOMEDLinks, semanticTypes):
     nodes = dict()
     links = dict()
     overlapLinks = set()
     for obj in jsonResp['response']['docs']:
         codes = stringToArr(obj['abstract_snomed_ents'])
+        seenCodes = set()
         for code in codes:
             if len(code) < 4:
                 break
             smID = re.sub(r'[^0-9]+', '', code[2].split(';')[-1].split(',')[0])
-            if smID in nodes:
-                nodes[smID]["timesSeen"] += 1
-            else:
-                newNode = {
-                    "name": re.sub(r'[^A-Za-z0-9\s]+', '', code[3]),
-                    "timesSeen": 1
-                }
-                nodes[smID] = newNode
+            smTypeCode = code[2].split(';')[1].split(',')[0]
+            # If not seen this result, handle and add to set
+            if smID not in seenCodes:
+                if smID in nodes:
+                    nodes[smID]["timesSeen"] += 1
+                else:
+                    newNode = {
+                        "name": re.sub(r'[^A-Za-z0-9\s]+', '', code[3]),
+                        "timesSeen": 1,
+                        "type": semanticTypes[smTypeCode]
+                    }
+                    nodes[smID] = newNode
+                seenCodes.add(smID)
+                
 
-
+        seenLinks = set()
         for idx, code in enumerate(codes):
             if len(code) < 4:
                 break
-            
             offset = idx - 1
             # Check backward 5
-            while offset > 0:
-                if int(code[0]) - int(codes[offset][1]) <= 5:
-                    smID1 = re.sub(r'[^0-9]+', '', code[2].split(';')[-1].split(',')[0])
-                    smID2 = re.sub(r'[^0-9]+', '', codes[offset][2].split(';')[-1].split(',')[0])
-                    link = (smID1, smID2)
+            while offset > 0 and int(code[0]) - int(codes[offset][1]) <= 5:
+                smID1 = re.sub(r'[^0-9]+', '', code[2].split(';')[-1].split(',')[0])
+                smID2 = re.sub(r'[^0-9]+', '', codes[offset][2].split(';')[-1].split(',')[0])
+                link = (smID1, smID2)
+                if link not in seenLinks:
                     if smID2 < smID1:
                         link = (smID2, smID1)
 
@@ -54,19 +68,20 @@ def loadJSONArray(jsonResp, SNOMEDLinks):
                         links[link] += 1
                     else:
                         links[link] = 1
+
                     if link in SNOMEDLinks:
                         overlapLinks.add(link)
-                    offset -= 1
-                else:
-                    break
+
+                    seenLinks.add(link)
+                offset -= 1
 
             offset = idx + 1
             # Check forward 5
-            while offset < len(codes):
-                if int(codes[offset][0]) - int(code[1]) <= 5:
-                    smID1 = re.sub(r'[^0-9]+', '', code[2].split(';')[-1].split(',')[0])
-                    smID2 = re.sub(r'[^0-9]+', '', codes[offset][2].split(';')[-1].split(',')[0])
-                    link = (smID1, smID2)
+            while offset < len(codes) and int(codes[offset][0]) - int(code[1]) <= 5:
+                smID1 = re.sub(r'[^0-9]+', '', code[2].split(';')[-1].split(',')[0])
+                smID2 = re.sub(r'[^0-9]+', '', codes[offset][2].split(';')[-1].split(',')[0])
+                link = (smID1, smID2)
+                if link not in seenLinks:
                     if smID2 < smID1:
                         link = (smID2, smID1)
 
@@ -74,10 +89,12 @@ def loadJSONArray(jsonResp, SNOMEDLinks):
                         links[link] += 1
                     else:
                         links[link] = 1
+
                     if link in SNOMEDLinks:
                         overlapLinks.add(link)
-                    offset += 1
-                else:
-                    break
 
+                    seenLinks.add(link)
+                offset += 1
+
+        print('process')
     return dictsToJSONResp(nodes, links, overlapLinks)
